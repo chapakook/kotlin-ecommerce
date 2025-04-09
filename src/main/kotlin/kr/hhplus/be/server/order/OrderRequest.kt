13ae6886caa3f7d70a43f004303e6fd7da141519
chapakook <chapakook96@gmail.com>
2025-04-09 22:19:57 +0900

    [REFACTOR] 이전 controller 제거 및 config 팩키지 위치변경

--- src/main/kotlin/kr/hhplus/be/server/order/OrderRequest.kt
+++
@@ -1,6 +0,0 @@
-package kr.hhplus.be.server.order
-
-data class OrderRequest(
-    val productId: Long,
-    val quantity: Int,
-)
---
+++ src/test/java/kr/hhplus/be/server/interfaces/point/PointControllerTest.kt
@@ -0,0 +1,16 @@
+package kr.hhplus.be.server.interfaces.point
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class PointControllerTest {
+
+@Test
+ fun point() {}
+
+@Test
+ fun charge() {}
+
+@Test
+ fun use() {}
+}
