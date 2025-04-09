64fce5956df20d7f292d6fe7979967acc349c9b9
chapakook <chapakook96@gmail.com>
2025-04-09 22:35:08 +0900

    [FIX] 포인트 컨트롤러 단위테스트 띄움처리 제거

--- src/test/java/kr/hhplus/be/server/interfaces/point/PointControllerTest.kt
+++ src/test/java/kr/hhplus/be/server/interfaces/point/PointControllerTest.kt
@@ -3,7 +3,6 @@ package kr.hhplus.be.server.interfaces.point
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Nested
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
