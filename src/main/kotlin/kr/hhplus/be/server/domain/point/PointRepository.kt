4b864d9e6f3d22fda3d0a0c2ee075f4226b20b3b
chapakook <chapakook96@gmail.com>
2025-04-10 09:30:04 +0900

    [FIX] domain/point -> domain/pointInfo 로 변경으로 인한 수정

--- src/main/kotlin/kr/hhplus/be/server/domain/point/PointRepository.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/point/PointRepository.kt
@@ -1,6 +1,6 @@
 package kr.hhplus.be.server.domain.point
 
 interface PointRepository {
-    fun findPointById(id: Long): Point
-    fun updatePointById(id: Long, amount: Long): Point
+    fun findPointById(id: Long): PointInfo
+    fun updatePointById(id: Long, amount: Long): PointInfo
 }
