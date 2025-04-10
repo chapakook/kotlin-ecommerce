b47f557eab81b71db595d81aba056d79c6df8d91
chapakook <chapakook96@gmail.com>
2025-04-10 09:59:07 +0900

    [FIX] domain/pointInfo 로 변경으로 인한 수정

--- src/test/java/kr/hhplus/be/server/domain/point/PointServiceTest.kt
+++ src/test/java/kr/hhplus/be/server/domain/point/PointServiceTest.kt
@@ -22,7 +22,7 @@ class PointServiceTest {
         fun `happy - getCmd 이용 getPoint 요청시 정상적으로 포인트를 가져온다`() {
             // given
             val getCmd = PointCommand.Get()
-            val fakePointInfo = PointInfo(1L,1L,100L)
+            val fakePointInfo = PointInfo.Point(1L,1L,100L)
             // when
             val result = pointService.getPoint(getCmd)
             // then
@@ -36,7 +36,7 @@ class PointServiceTest {
         fun `happy - chargeCmd 이용 chargePoint 요청시 정상적으로 포인트 충전을 한다`() {
             // given
             val chargeCmd = PointCommand.Charge()
-            val fakePointInfo = PointInfo(1L,1L,100L)
+            val fakePointInfo = PointInfo.Point(1L,1L,100L)
             // when
             val result = pointService.chargePoint(chargeCmd)
             // then
@@ -50,7 +50,7 @@ class PointServiceTest {
         fun `happy - useCmd 이용 usePoint 요처시 정상적인 포인트 사용을 한다`() {
             // given
             val useCmd = PointCommand.Use()
-            val fakePointInfo = PointInfo(1L,1L,100L)
+            val fakePointInfo = PointInfo.Point(1L,1L,100L)
             // when
             val result = pointService.usePoint(useCmd)
             // then
