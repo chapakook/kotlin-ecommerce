deed354850ee481957cca174bb4573a2f11c121a
chapakook <chapakook96@gmail.com>
2025-04-11 00:28:53 +0900

    [FIX] 상품인포 init 체크 제거

--- src/main/kotlin/kr/hhplus/be/server/domain/product/ProductInfo.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/product/ProductInfo.kt
@@ -6,23 +6,10 @@ class  ProductInfo{
         val name: String,
         val price: Long,
         val stock: Long,
-    ){
-        init {
-            require(productId > 0) { "productId must be positive" }
-            require(name.isNotEmpty()) { "name must not be empty" }
-            require(price > 0) { "price must be positive" }
-            require(stock >= 0) { "stock must be not negative" }
-        }
-    }
-
+    )
     data class Rank(
         val productId: Long,
         val rank: Int,
         val quantity: Int,
-    ){
-        init {
-            require(productId > 0) { "productId must be positive" }
-            require(rank > 0) { "rank must be positive" }
-        }
-    }
+    )
 }
