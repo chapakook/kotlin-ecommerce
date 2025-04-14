c7a30c621317b53152bb32fe44e216d2d0554913
chapakook <chapakook96@gmail.com>
2025-04-15 01:21:40 +0900

    [REFACTOR] interfaces/coupon 리펙토링

--- src/main/kotlin/kr/hhplus/be/server/interfaces/coupon/CouponController.kt
+++ src/main/kotlin/kr/hhplus/be/server/interfaces/coupon/CouponController.kt
@@ -1,6 +1,7 @@
 package kr.hhplus.be.server.interfaces.coupon
 
 import kr.hhplus.be.server.domain.coupon.CouponService
+import kr.hhplus.be.server.interfaces.coupon.CouponResponse.IssuedCoupon
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/coupon")
-class CouponController (
-    private val couponService: CouponService
+class CouponController(
+    private val couponService: CouponService,
 ) {
     @PostMapping("/issue")
-    fun issue(@RequestBody req: CouponRequest.Issue): CouponResponse.Coupon? {
+    fun issue(@RequestBody req: CouponRequest.Issue): IssuedCoupon? {
         val coupon = couponService.issue(req.toCouponCmdIssue())
-        return if (coupon != null) CouponResponse().ofCoupon(coupon) else null
+        return if (coupon != null) IssuedCoupon.of(coupon) else null
     }
 }
--- src/main/kotlin/kr/hhplus/be/server/interfaces/coupon/CouponResponse.kt
+++ src/main/kotlin/kr/hhplus/be/server/interfaces/coupon/CouponResponse.kt
@@ -4,6 +4,12 @@ import kr.hhplus.be.server.domain.coupon.CouponInfo
 import java.time.LocalDateTime
 
 class CouponResponse {
-    data class Coupon(val couponId: Long, val expiredAt: LocalDateTime)
-    fun ofCoupon(coupon: CouponInfo.UserCoupon): Coupon = with(coupon){ Coupon(couponId, expiredAt) }
+    data class IssuedCoupon(
+        val couponId: Long,
+        val expiredAt: LocalDateTime,
+    ) {
+        companion object {
+            fun of(coupon: CouponInfo.UserCoupon): IssuedCoupon = with(coupon) { IssuedCoupon(couponId, expiredAt) }
+        }
+    }
 }
