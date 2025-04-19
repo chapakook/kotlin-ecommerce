f8394f64a435c062bd8a6ad13997626ddaa3920c
chapakook <chapakook96@gmail.com>
2025-04-19 17:54:48 +0900

    [FEAT] coupon test case 추가 - 쿠폰 발생시 만료일자 체크

--- src/main/kotlin/kr/hhplus/be/server/domain/coupon/Coupon.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/coupon/Coupon.kt
@@ -1,8 +1,7 @@
 package kr.hhplus.be.server.domain.coupon
 
 import jakarta.persistence.*
-import kr.hhplus.be.server.support.ErrorCode.EXPIRED_COUPON
-import kr.hhplus.be.server.support.ErrorCode.USED_COUPON
+import kr.hhplus.be.server.support.ErrorCode.*
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -47,21 +46,33 @@ class Coupon(
             type: CouponType,
             value: Long,
             expiryMillis: Long,
-        ): Coupon = Coupon(
-            0L,
-            userId,
-            type,
-            value,
-            expiryMillis,
-            LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli(),
-            LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli(),
-            true
-        )
+        ): Coupon {
+            require(isAfter(expiryMillis)) { EXPIRED_COUPON_EVENT.message }
+            val toDay = LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli()
+            return Coupon(
+                0L,
+                userId,
+                type,
+                value,
+                expiryMillis,
+                toDay,
+                toDay,
+                true
+            )
+        }
+
+        private fun isBefore(millis: Long): Boolean =
+            !LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), ZoneOffset.UTC).toLocalDate()
+                .isAfter(LocalDate.now(ZoneOffset.UTC))
+
+        private fun isAfter(millis: Long): Boolean =
+            !LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), ZoneOffset.UTC).toLocalDate()
+                .isBefore(LocalDate.now(ZoneOffset.UTC))
     }
 
     fun use(amount: Long): Long {
         require(isActive) { USED_COUPON.message }
-        require(validExpiry()) { EXPIRED_COUPON.message }
+        require(isAfter(expiryMillis)) { EXPIRED_COUPON.message }
         isActive to true
         updateMillis = LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli()
         return when (type) {
@@ -69,8 +80,4 @@ class Coupon(
             CouponType.RATE -> (amount - (amount * (value / 100.0))).toLong()
         }
     }
-
-    private fun validExpiry(): Boolean =
-        LocalDateTime.ofInstant(Instant.ofEpochMilli(expiryMillis), ZoneOffset.UTC).toLocalDate()
-            .isAfter(LocalDate.now(ZoneOffset.UTC))
 }
--- src/test/java/kr/hhplus/be/server/domain/coupon/CouponTest.kt
+++ src/test/java/kr/hhplus/be/server/domain/coupon/CouponTest.kt
@@ -1,88 +1,129 @@
 package kr.hhplus.be.server.domain.coupon
 
 import kr.hhplus.be.server.support.ErrorCode
+import kr.hhplus.be.server.support.ErrorCode.EXPIRED_COUPON_EVENT
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
 class CouponTest {
-    @Test
-    fun `happy - 쿠폰을 발급할 수 있다`() {
-        // given
-        val userId = 123L
-        val type = CouponType.FIXED
-        val value = 1000L
-        val expiryMillis = LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
-        // when
-        val coupon = Coupon.issue(userId, type, value, expiryMillis)
-        // then
-        assertThat(coupon)
-            .extracting("userId", "type", "value", "isActive")
-            .containsExactly(userId, type, value, true)
+
+    @Nested
+    inner class Issue {
+        @Test
+        fun `happy - 쿠폰을 발급할 수 있다`() {
+            // given
+            val userId = 123L
+            val type = CouponType.FIXED
+            val value = 1000L
+            val expiryMillis = LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
+            // when
+            val coupon = Coupon.issue(userId, type, value, expiryMillis)
+            // then
+            assertThat(coupon)
+                .extracting("userId", "type", "value", "isActive")
+                .containsExactly(userId, type, value, true)
+        }
+
+        @Test
+        fun `happy - 당일 쿠폰을 발급할 수 있다`() {
+            // given
+            val userId = 123L
+            val type = CouponType.FIXED
+            val value = 1000L
+            val expiryMillis = LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli()
+            // when
+            val coupon = Coupon.issue(userId, type, value, expiryMillis)
+            // then
+            assertThat(coupon)
+                .extracting("userId", "type", "value", "isActive")
+                .containsExactly(userId, type, value, true)
+        }
+
+        @Test
+        fun `bad - 오늘보다 이전의 만료일은 발급하려고 하면 IllegalArgumentException을 반환한다`() {
+            // given
+            val userId = 1234L
+            val type = CouponType.FIXED
+            val value = 1000L
+            val expiryMillis = LocalDateTime.now().minusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
+            // when & then
+            val exception = assertThrows<IllegalArgumentException> { Coupon.issue(userId, type, value, expiryMillis) }
+            assertThat(exception.message).isEqualTo(EXPIRED_COUPON_EVENT.message)
+        }
     }
 
-    @Test
-    fun `happy - 정액 쿠폰이 정상 작동한다`() {
-        // given
-        val coupon = Coupon.issue(
-            123L,
-            CouponType.FIXED,
-            500L,
-            LocalDateTime.now().plusDays(20).toInstant(ZoneOffset.UTC).toEpochMilli()
-        )
-        // when
-        val remainingAmount = coupon.use(1000L)
-        // then
-        assertThat(remainingAmount).isEqualTo(500L)
-        assertThat(coupon.isActive).isTrue()
-        assertThat(coupon.updateMillis).isPositive()
-    }
+    @Nested
+    inner class Use {
+        @Test
+        fun `happy - 정액 쿠폰이 정상 적용된다`() {
+            // given
+            val coupon = Coupon.issue(
+                123L,
+                CouponType.FIXED,
+                500L,
+                LocalDateTime.now().plusDays(20).toInstant(ZoneOffset.UTC).toEpochMilli()
+            )
+            // when
+            val remainingAmount = coupon.use(1000L)
+            // then
+            assertThat(remainingAmount).isEqualTo(500L)
+            assertThat(coupon.isActive).isTrue()
+            assertThat(coupon.updateMillis).isPositive()
+        }
 
-    @Test
-    fun `happy - 정률 쿠폰이 정상 적용된다`() {
-        // given
-        val coupon = Coupon.issue(
-            123L,
-            CouponType.RATE,
-            20L,
-            LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
-        )
-        // when
-        val remainingAmount = coupon.use(1000L)
-        // then
-        assertThat(remainingAmount).isEqualTo(800L) // 20% discount on 1000 is 200
-        assertThat(coupon.isActive).isTrue()
-        assertThat(coupon.updateMillis).isPositive()
-    }
+        @Test
+        fun `happy - 정률 쿠폰이 정상 적용된다`() {
+            // given
+            val coupon = Coupon.issue(
+                123L,
+                CouponType.RATE,
+                20L,
+                LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
+            )
+            // when
+            val remainingAmount = coupon.use(1000L)
+            // then
+            assertThat(remainingAmount).isEqualTo(800L) // 20% discount on 1000 is 200
+            assertThat(coupon.isActive).isTrue()
+            assertThat(coupon.updateMillis).isPositive()
+        }
 
-    @Test
-    fun `bad - 만료된 쿠폰은 사용시 IllegalArgumentException 을 반환한다`() {
-        // given
-        val coupon = Coupon.issue(
-            123L,
-            CouponType.FIXED,
-            500L,
-            LocalDateTime.now().minusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
-        )
-        // when & then
-        val exception = assertThrows<IllegalArgumentException> { coupon.use(1000L) }
-        assertThat(exception.message).isEqualTo(ErrorCode.EXPIRED_COUPON.message)
-    }
+        @Test
+        fun `bad - 만료된 쿠폰은 사용시 IllegalArgumentException 을 반환한다`() {
+            // given
+            val date = LocalDateTime.now().minusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
+            val coupon = Coupon(
+                123L,
+                444L,
+                CouponType.FIXED,
+                500L,
+                date,
+                date,
+                date,
+                true
+            )
+            // when & then
+            val exception = assertThrows<IllegalArgumentException> { coupon.use(1000L) }
+            assertThat(exception.message).isEqualTo(ErrorCode.EXPIRED_COUPON.message)
+        }
 
-    @Test
-    fun `bad - 사용된 쿠폰은 사용시 IllegalArgumentException 반환한다`() {
-        // given
-        val coupon = Coupon.issue(
-            123L,
-            CouponType.FIXED,
-            500L,
-            LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
-        )
-        coupon.isActive = false
-        // when & then
-        val exception = assertThrows<IllegalArgumentException> { coupon.use(1000L) }
-        assertThat(exception.message).isEqualTo(ErrorCode.USED_COUPON.message)
+        @Test
+        fun `bad - 사용된 쿠폰은 사용시 IllegalArgumentException 반환한다`() {
+            // given
+            val coupon = Coupon.issue(
+                123L,
+                CouponType.FIXED,
+                500L,
+                LocalDateTime.now().plusDays(1).toInstant(ZoneOffset.UTC).toEpochMilli()
+            )
+            coupon.isActive = false
+            // when & then
+            val exception = assertThrows<IllegalArgumentException> { coupon.use(1000L) }
+            assertThat(exception.message).isEqualTo(ErrorCode.USED_COUPON.message)
+        }
     }
 }
