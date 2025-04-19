1ffcae8424c97516eaa410b7cebf58bff6ee2270
chapakook <chapakook96@gmail.com>
2025-04-19 17:54:17 +0900

    [REFACTOR] ErrorCode 재정의

--- src/main/kotlin/kr/hhplus/be/server/support/ErrorCode.kt
+++ src/main/kotlin/kr/hhplus/be/server/support/ErrorCode.kt
@@ -4,22 +4,25 @@ enum class ErrorCode(val code: String, val message: String) {
     // E0xx general error
     USER_NOT_FOUND("E000", "사용자를 찾을 수 없습니다."),
 
-    // E2xx point error
+    // E1xx point error
     AMOUNT_MUST_BE_POSITIVE("E100", "충전 포인트는 음수일 수 없습니다."),
     OUT_OF_POINT("E101", "포인트가 부족합니다."),
 
-    // E3xx product error
+    // E2xx product error
     QUANTITY_MUST_BE_POSITIVE("E200", "재고차감 수량이 잘못됐습니다."),
     OUT_OF_STOCK("E201", "상품의 재고가 부족합니다."),
     STOCK_NOT_FOUND("E202", "상품의 재고를 찾을 수 없습니다."),
     POPULARITY_NOT_FOUNT("E203", "인기상품이 집계되지 않았습니다. 잠시만 기달려주세요."),
 
-    // E4xx coupon error
-    MAX_ISSUED_COUPON("E300", "선착순 쿠폰이 모두 발급되었습니다."),
-    USED_COUPON("E301", "이미 사용된 쿠폰입니다."),
-    EXPIRED_COUPON("E302", "만료된 쿠폰입니다."),
+    // E3xx coupon error
     COUPON_EVENT_NOT_FOUND("E303", "쿠폰이벤트를 찾을 수 없습니다."),
-    COUPON_NOT_FOUND("E304", "쿠폰을 찾을 수 없습니다."),
+    EXPIRED_COUPON_EVENT("E305", "만료된 쿠폰이벤트 입니다"),
+
+    // E4xx coupon event error
+    MAX_ISSUED_COUPON("E400", "선착순 쿠폰이 모두 발급되었습니다."),
+    USED_COUPON("E401", "이미 사용된 쿠폰입니다."),
+    EXPIRED_COUPON("E402", "만료된 쿠폰입니다."),
+    COUPON_NOT_FOUND("E404", "쿠폰을 찾을 수 없습니다."),
 
     // TE*** test error
     CREATE_BASE_POINT("TE001", "포인트 초깃값은 양수여야 합니다.")
