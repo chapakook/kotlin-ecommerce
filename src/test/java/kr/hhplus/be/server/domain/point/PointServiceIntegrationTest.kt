e32be42283c0f2107e23bb43a598c510dcd41214
chapakook <chapakook96@gmail.com>
2025-04-22 00:30:58 +0900

    [FEAT] point service 충전/사용 동시성 테스트 추가

--- src/test/java/kr/hhplus/be/server/domain/point/PointServiceIntegrationTest.kt
+++ src/test/java/kr/hhplus/be/server/domain/point/PointServiceIntegrationTest.kt
@@ -1,14 +1,15 @@
 package kr.hhplus.be.server.domain.point
 
-import jakarta.transaction.Transactional
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 
 @SpringBootTest
-@Transactional
 class PointServiceIntegrationTest {
     @Autowired
     private lateinit var pointRepository: PointRepository
@@ -46,6 +47,39 @@ class PointServiceIntegrationTest {
             assertThat(result.userId).isEqualTo(userId)
             assertThat(result.balance).isEqualTo(expected.balance + amount)
         }
+
+        @Test
+        fun `happy - 포인트 충전 동시성 테스트`() {
+            // given
+            val userId = 4L
+            val amount = 100L
+            val count = 10
+            val service = Executors.newFixedThreadPool(5)
+            val latch = CountDownLatch(count)
+            val expected = pointService.find(PointCommand.Find(userId))
+            val success = AtomicInteger(0)
+            val failure = AtomicInteger(0)
+            // when
+            repeat(count) {
+                service.submit {
+                    try {
+                        pointService.charge(PointCommand.Charge(userId, amount))
+                        success.incrementAndGet()
+                    } catch (e: Exception) {
+                        failure.incrementAndGet()
+                    } finally {
+                        latch.countDown()
+                    }
+                }
+            }
+            latch.await()
+            service.shutdown()
+            val result = pointService.find(PointCommand.Find(userId))
+            // then
+            assertThat(success.get()).isGreaterThanOrEqualTo(count)
+            assertThat(failure.get()).isLessThanOrEqualTo(0)
+            assertThat(result.balance).isEqualTo(expected.balance + amount * success.get())
+        }
     }
 
     @Nested
@@ -62,6 +96,39 @@ class PointServiceIntegrationTest {
             // then
             assertThat(result.userId).isEqualTo(userId)
             assertThat(result.balance).isEqualTo(expected.balance - amount)
+        }
+
+        @Test
+        fun `happy - 포인트 사용 동시성 테스트`() {
+            // given
+            val userId = 5L
+            val amount = 100L
+            val count = 10
+            val service = Executors.newFixedThreadPool(5)
+            val latch = CountDownLatch(count)
+            val expected = pointService.find(PointCommand.Find(userId))
+            val success = AtomicInteger(0)
+            val failure = AtomicInteger(0)
+            // when
+            repeat(count) {
+                service.submit {
+                    try {
+                        pointService.use(PointCommand.Use(userId, amount))
+                        success.incrementAndGet()
+                    } catch (e: Exception) {
+                        failure.incrementAndGet()
+                    } finally {
+                        latch.countDown()
+                    }
+                }
+            }
+            latch.await()
+            service.shutdown()
+            val result = pointService.find(PointCommand.Find(userId))
+            // then
+            assertThat(success.get()).isGreaterThanOrEqualTo(count)
+            assertThat(failure.get()).isLessThanOrEqualTo(0)
+            assertThat(result.balance).isEqualTo(expected.balance - amount * success.get())
         }
     }
 }
