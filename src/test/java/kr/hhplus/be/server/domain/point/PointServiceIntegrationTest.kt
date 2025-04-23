ba3ae567fa7ba05ce5a27ccb56f0e579cda3a390
chapakook <chapakook96@gmail.com>
2025-04-23 09:20:34 +0900

    [FIX] point service test

--- src/test/java/kr/hhplus/be/server/domain/point/PointServiceIntegrationTest.kt
+++ src/test/java/kr/hhplus/be/server/domain/point/PointServiceIntegrationTest.kt
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class PointServiceIntegrationTests {
+class PointServiceIntegrationTest {
 
     @Autowired
     private lateinit var pointService: PointService
