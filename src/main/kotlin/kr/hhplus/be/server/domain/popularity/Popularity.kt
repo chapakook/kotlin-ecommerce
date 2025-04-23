16972b5f3497048ba4f55ef27a7a09de2c182033
chapakook <chapakook96@gmail.com>
2025-04-24 08:54:05 +0900

    [FIX] popularityEntity 제거 -> JPA 객체 사용하기위함

--- src/main/kotlin/kr/hhplus/be/server/domain/popularity/Popularity.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/popularity/Popularity.kt
@@ -1,10 +1,21 @@
 package kr.hhplus.be.server.domain.popularity
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "popularises")
 class Popularity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val popularityId: Long,
+    @Column(nullable = false)
     val productId: Long,
+    @Column(nullable = false)
     val productName: String,
+    @Column(name = "`rank`", nullable = false)
     val rank: Int,
+    @Column(nullable = false)
     val totalOrder: Int,
+    @Column(nullable = false)
     val updateMills: Long,
 )
--- src/main/kotlin/kr/hhplus/be/server/infrastructure/popularity/PopularityJPARepository.kt
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/popularity/PopularityJPARepository.kt
@@ -1,12 +1,13 @@
 package kr.hhplus.be.server.infrastructure.popularity
 
+import kr.hhplus.be.server.domain.popularity.Popularity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface PopularityJPARepository : JpaRepository<PopularityEntity, Long> {
-    @Query("SELECT p FROM PopularityEntity p ORDER BY p.rank DESC")
-    fun findTop5ByOrderByRankDesc(): List<PopularityEntity>
-    fun save(popularity: PopularityEntity): PopularityEntity
+interface PopularityJPARepository : JpaRepository<Popularity, Long> {
+    @Query("SELECT p FROM Popularity p ORDER BY p.rank DESC")
+    fun findTop5ByOrderByRankDesc(): List<Popularity>
+    fun save(popularity: Popularity): Popularity
 }
