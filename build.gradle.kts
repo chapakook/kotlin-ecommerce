5e132d1bc681e8a10c42e42ccb4bb659a102d57f
chapakook <chapakook96@gmail.com>
2025-05-30 06:59:44 +0900

    [FIX] build.gradle.kts 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     // Kafka
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
